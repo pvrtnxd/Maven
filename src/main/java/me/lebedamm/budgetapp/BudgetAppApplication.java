6ad6e554600e32b6c45c8e7c411bd84c0e962996
lebed <lebedamm@gmail.com>
2023-02-04 15:40:24 +0300

    Задание

---
+++ src/main/java/me/lebedamm/budgetapp/BudgetAppApplication.java
@@ -0,0 +1,16 @@
+package me.lebedamm.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class BudgetAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAppApplication.class, args);
+
+
+    }
+
+}
---
+++ src/main/java/me/lebedamm/budgetapp/controllers/FirstController.java
@@ -0,0 +1,19 @@
+package me.lebedamm.budgetapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FirstController {
+
+    @GetMapping
+    public String HelloWeb() {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String GetInfo() {
+        return "Лебедева Марина, CookingApp, 4.02.2023, Книга рецептов";
+    }
+}
+
