c5c0a42a0be58b0ec17d16ea4cb6fab84b389f4d
lebed <lebedamm@gmail.com>
2023-02-04 15:41:18 +0300

    Задание

--- src/main/java/me/lebedamm/budgetapp/controllers/FirstController.java
+++ src/main/java/me/lebedamm/budgetapp/controllers/FirstController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
 
     @GetMapping
-    public String HelloWeb() {
+    public String Message() {
         return "Приложение запущено";
     }
     @GetMapping("/info")
